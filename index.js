220bbbc2526ad2acb8c4c2b977da9b6615249d50
nidjasim <polamekkartnija@gmail.com>
2026-01-04 21:43:01 +0100

    exp_1

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const startBtn = document.getElementById('activate');
-const startWrap = document.querySelector('.front-img');
+// const startBtn = document.getElementById('activate');
+// const startWrap = document.querySelector('.front-img');
 const mainRoom = document.querySelector('.room');
 
-startBtn.addEventListener('click', ()=>{
-    startWrap.classList.add('hide');
-    mainRoom.classList.add('start');
-})
+// startBtn.addEventListener('click', ()=>{
+//     startWrap.classList.add('hide');
+//     mainRoom.classList.add('start');
+// })
+
+// mainRoom.addEventListener('mousemove', (e)=>{
+//     const x = e.pageX / 10;
+//     const y = e.pageY / 10;
+//     console.log(x, y)
+//     mainRoom.style.perspectiveOrigin = `${x}% ${y}%`;
+// })
